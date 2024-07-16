724ad8ad4620cdb1bedcf83caff7cc3605512bd4
Martin Walther <it@martin-walther.ch>
2024-07-16 18:39:24 +0200

    Update build.ps1

--- pode/build.ps1
+++ pode/build.ps1
@@ -1,6 +1,8 @@
 # Build container from docker file
 Push-Location -Path $PSScriptRoot
 
+# git clone https://github.com/tinuwalther/PodePSHTML.git ./PodePSHTML
+
 $hostName      = 'PodePSHTML'
 $containerName = 'PodePSHTML'
 $imageName     = 'tinuwalther/pode'
@@ -23,8 +25,8 @@ if([string]::IsNullOrEmpty($image)){
 
 if([string]::IsNullOrEmpty($container)){
     Write-Host "Run and start container $($containerName)"
-    # docker run -e TZ="Europe/Zurich" --hostname $hostName --name $containerName --network custom -it $imageName
-    docker run -e TZ="Europe/Zurich" --hostname $hostName --name $containerName --network custom -p 8080:8080 -d $imageName
+    # docker run -e TZ="Europe/Zurich" --hostname $hostName --name $containerName --network $networkName -it $imageName
+    docker run -e TZ="Europe/Zurich" --hostname $hostName --name $containerName --network $networkName -p 8080:8080 -d $imageName
 }else{
     $isrunning = docker ps --filter "name=$containerName" --format "{{.Names}}"
     if([String]::IsNullOrEmpty($isrunning)){
@@ -37,7 +39,7 @@ if([string]::IsNullOrEmpty($container)){
 docker inspect --format='{{json .NetworkSettings.Networks.custom }}' $containerName | ConvertFrom-Json
 
 <#
-Invoke-WebRequest -Uri 'http://nexus:8081/'
+Invoke-WebRequest -Uri 'http://localhost:8080/'
 
 Invoke-WebRequest -Uri 'http://nexus:8081/repository/PSModules/'
 
