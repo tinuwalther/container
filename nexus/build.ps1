3accb7f2068f706997f72f1e51a63f41bcb5685b
Martin Walther <it@martin-walther.ch>
2024-07-17 14:04:23 +0200

    Update build.ps1

--- nexus/build.ps1
+++ nexus/build.ps1
@@ -4,8 +4,10 @@ Push-Location -Path $PSScriptRoot
 $containerName = 'nexus'
 $imageName     = 'sonatype/nexus3'
 $networkName   = 'custom'
+$containerData = 'nexus-data'
 
 $network   = docker network ls --filter "name=$networkName" --format "{{.Name}}"
+$volume    = docker volume ls --filter "name=$containerData" --format "{{.Name}}"
 $image     = docker images $imageName --format "{{.Repository}}"
 $container = docker ps -a --filter "name=$containerName" --format "{{.Names}}"
 
@@ -15,6 +17,12 @@ if([string]::IsNullOrEmpty($network)){
 }
 docker network inspect $networkName --format='{{json .IPAM.Config }}' | ConvertFrom-Json | Format-List
 
+if([string]::IsNullOrEmpty($volume)){
+    Write-Host "Create volume $($volume)"
+    docker volume create $containerData
+}
+docker volume inspect $containerData --format='{{json .Mountpoint }}' | ConvertFrom-Json | Format-List
+
 if([string]::IsNullOrEmpty($image)){
     Write-Host "Download image $($imageName)"
     docker pull $imageName
@@ -22,7 +30,7 @@ if([string]::IsNullOrEmpty($image)){
 
 if([string]::IsNullOrEmpty($container)){
     Write-Host "Run and start container $($containerName)"
-    docker run -e TZ="Europe/Zurich"  -d -p 8081:8081 --name $containerName --network custom -it $imageName
+    docker run -e TZ="Europe/Zurich"  -d -p 8081:8081 --name $containerName --network $networkName -it $imageName
 }else{
     Write-Host "Start container $($containerName)"
     docker start $containerName
